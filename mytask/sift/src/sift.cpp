079b1b9c44e4348ee33a8a60308ed109a7445a63
ROWAN0620 <cliche0620@gmail.com>
2019-11-01 07:33:03 +0000

    detect [1/11/2019]

--- mytask/sift/src/sift.cpp
+++ mytask/sift/src/sift.cpp
@@ -4,6 +4,7 @@
 //#include <opencv2/nonfree/nonfree.hpp>
 //#include <opencv2/nonfree/features2d.hpp>
 #include <opencv2/features2d/features2d.hpp>
+#include <opencv2/xfeatures2d/nonfree.hpp>
 #include <iostream>
 #include <cstdlib>
 #include <opencv2/highgui/highgui.hpp>
